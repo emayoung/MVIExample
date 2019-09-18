1515c21d0dfa1b3279afd189e1613e0032f34d55
Emayoung <emayoung95@gmail.com>
2019-09-18 10:34:03 +0100

    Update json data

--- remote/src/main/java/com/threedee/remote/model/BufferooModel.kt
+++ remote/src/main/java/com/threedee/remote/model/BufferooModel.kt
@@ -3,4 +3,4 @@ package com.threedee.remote.model
 /**
  * Representation for a [BufferooModel] fetched from the API
  */
-class nBufferooModel(val id: Long, val name: String, val title: String, val avatar: String)
+class BufferooModel(val id: Long, val name: String, val title: String, val avatar: String)
